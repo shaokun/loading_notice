478cdfaaa9cbc14963866018b2ec4b10beca6815
Thomas Duerr <thomduerr@gmail.com>
2009-07-28 00:28:38 +0200

    using prototype Event.observe for observing scroll and resize of the window object

--- javascripts/loading_notice.js
+++ javascripts/loading_notice.js
@@ -1,14 +1,14 @@
 var LoadingNotice = Class.create({
-	initialize: function() {
-		this.loaded = false;
-		this.loading = this.buildLoader();
-		
-		Ajax.Responders.register({
-	    	"onCreate": this.startLoading.bind(this),
-	    	"onComplete": this.stopLoading.bind(this),
-			"onException": this.stopLoading.bind(this)
-	  	});	
-	},
+    initialize: function() {
+        this.loaded = false;
+        this.loading = this.buildLoader();
+
+        Ajax.Responders.register({
+            "onCreate": this.startLoading.bind(this),
+            "onComplete": this.stopLoading.bind(this),
+            "onException": this.stopLoading.bind(this)
+        });	
+    },
 
     buildLoader: function(){
         var loader = new Element('div', {'id': 'loading'}).update("Loading ...").hide();
@@ -16,39 +16,38 @@ var LoadingNotice = Class.create({
         return loader;
     },
 
-  	startLoading: function() {
-    	this.loaded = false;
-    	window.setTimeout(this.showLoading.bind(this), 200);
-  	},
+    startLoading: function() {
+        this.loaded = false;
+        window.setTimeout(this.showLoading.bind(this), 200);
+    },
 
-  	showLoading: function() {
-    	if (!this.loaded) {
-			this.positionLoading();
-	       	new Effect.Appear(this.loading.id, {duration: 0.5});
-	
-			// document.observe('resize', this.positionLoading.bind(this));
-			// document.observe('scroll', this.positionLoading.bind(this));
-			window.onresize = this.positionLoading.bind(this);
-			window.onscroll = this.positionLoading.bind(this);
-    	}
-  	},
+    showLoading: function() {
+        if (!this.loaded) {
+            this.positionLoading();
+            new Effect.Appear(this.loading, {duration: 0.4});
 
-	positionLoading: function() {
-		var top = document.viewport.getScrollOffsets().top + 1;
-		var page_width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-		var left = (document.viewport.getScrollOffsets().left + ((page_width - this.loading.getWidth())) / 2);
-		
-		this.loading.style.top = top + 'px';
-		this.loading.style.left = left + 'px';
-	},
+            this.bindedPositionLoading = this.positionLoading.bind(this);
+            Event.observe(window, 'resize', this.bindedPositionLoading);
+            Event.observe(window, 'scroll', this.bindedPositionLoading);
+        }
+    },
 
-  	stopLoading: function() {
-    	this.loading.hide();
-    	this.loaded = true;
+    positionLoading: function() {
+        var top = document.viewport.getScrollOffsets().top + 1;
+        var page_width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+        var left = (document.viewport.getScrollOffsets().left + ((page_width - this.loading.getWidth())) / 2);
 
-		window.onresize = null;
-		window.onscroll = null;
-  	}
+        this.loading.style.top = top + 'px';
+        this.loading.style.left = left + 'px';
+    },
+
+    stopLoading: function() {
+        this.loading.hide();
+        this.loaded = true;
+
+        Event.stopObserving(window, 'resize', this.bindedPositionLoading);
+        Event.stopObserving(window, 'scroll', this.bindedPositionLoading);
+    }
 });
 
 document.observe("dom:loaded", function() {
